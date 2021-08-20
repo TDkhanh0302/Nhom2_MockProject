2f501a9d0b33fb69ce09b12f0a6d5fd1a3a63dbf
khanh <baokhanhtlc1902@gmail.com>
2021-08-20 16:25:06 +0700

    xay dung context va call api

--- mockproject-nhom2/src/App.js
+++ mockproject-nhom2/src/App.js
@@ -1,14 +1,13 @@
-import Home from './Pages/Home/Home'
 import './App.css';
-import LoginForm from './Pages/Login/LoginForm';
-import SignUpForm from './Pages/Login/SignUpForm';
-import NewTournament from './NewTournament/NewTournament';
+import TournamentsContextProvider from './contexts/TournamentsContext';
+import NewTournament from './Pages/NewTournament/NewTournament';
 
 function App() {
   return (
     <div className="App">
-      <SignUpForm/>
-      <NewTournament />;
+      <TournamentsContextProvider>
+        <NewTournament />;
+      </TournamentsContextProvider>
     </div>
   );
 }
