42a3e01efb5a503b17f20afcff77123fcdfd76e7
thanhdogn <62937597+thanhdogn@users.noreply.github.com>
2021-08-20 09:41:13 +0700

    fix consflix

--- mockproject-nhom2/src/Pages/Login/LoginForm.jsx
+++ mockproject-nhom2/src/Pages/Login/LoginForm.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './style.css'
 import Header from '../../Component/Header/Header'
-import Footer from '../../Component/Footer/Footer'
 const LoginForm = () =>{
     return(
         <>
-        
         <Header/>
         {
           <div className='loginForm'>
@@ -22,15 +20,15 @@ const LoginForm = () =>{
                 <form>
                   <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" required />
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
                   </div>
                   <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" required />
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                     <label className="form-check-label" htmlFor="exampleCheck1">Remember me!</label>
                   </div>
                   <button type="submit" className="btn btn-submit-form">Submit</button>
@@ -40,12 +38,7 @@ const LoginForm = () =>{
           </div>
           </div>
         }
-<<<<<<< HEAD
       </>
-=======
-        <Footer/>
-     </>
->>>>>>> 9111405105519b1d30d1767b21e0e88b9b5bd13c
     );
 }
 export default LoginForm;
