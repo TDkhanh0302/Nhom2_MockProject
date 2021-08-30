d4cb2e6b02d2564f8f7a995cc0c3dee5009ce7a9
khanh <baokhanhtlc1902@gmail.com>
2021-08-30 21:41:08 +0700

    fix validate form signup

--- mockproject-nhom2/src/Component/Brackets/brackets.js
+++ mockproject-nhom2/src/Component/Brackets/brackets.js
@@ -3,11 +3,7 @@ import { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../../../node_modules/jquery-bracket/dist/jquery.bracket.min.js';
 import { TournamentsContext } from '../../contexts/TournamentsContext.jsx';
-<<<<<<< HEAD
-import './styles.css'
-=======
 import './styles.css';
->>>>>>> 01ae4d686ea3446f77e397563c36df37ebd2c664
 
 function Bracket(props) {
   const { tournamentId, currentTour } = props;
@@ -150,11 +146,7 @@ function Bracket(props) {
   }, []);
 
   $(function () {
-<<<<<<< HEAD
-    if(!userLogged || currentTour?.user_id !== userLogged.id){
-=======
     if (!userLogged || currentTour?.user_id !== userLogged.id) {
->>>>>>> 01ae4d686ea3446f77e397563c36df37ebd2c664
       console.log(customData);
       $('#matches .demo').bracket({
         init: customData,
